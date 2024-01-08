e6012428e5bc231317b31097dbddbd2332b33ff8
Cruiz102 <cruiznavarro44@gmail.com>
2024-01-08 13:51:24 -0400

    add the subscribers in the ros_deps.cpp

--- src/ROS/ros_deps.cpp
+++ src/ROS/ros_deps.cpp
@@ -15,7 +15,14 @@ void initROSDependencies(void)
     nh.advertise(current_depth_pub);
 
     // Declare subscribers
-    nh.subscribe(mov_motor_sub);
+    nh.subscribe(thruster_sub_1);
+    nh.subscribe(thruster_sub_2);
+    nh.subscribe(thruster_sub_3);
+    nh.subscribe(thruster_sub_4);
+    nh.subscribe(thruster_sub_5);
+    nh.subscribe(thruster_sub_6);
+    nh.subscribe(thruster_sub_7);
+    nh.subscribe(thruster_sub_8);
     nh.subscribe(camera_angle_sub);
     nh.subscribe(gripper_mode_sub);
 }
